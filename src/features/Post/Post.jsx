27da7352319248663ee9dfdbb52e281a784d986f
mattsteen14 <mattsteen14@me.com>
2024-06-19 12:32:53 +0100

    import arrows into Post

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import './Post.css';
+import {
+    TiArrowUp,
+    TiArrowUpOutline,
+    TiArrowUpThick,
+    TiArrowDown,
+    TiArrowDownOutline,
+    TiArrowDownThick,
+    TiMessage
+} from 'react-icons/ti';
 
 export default function Post() {
   return (
