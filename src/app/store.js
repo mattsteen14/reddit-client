f1deaaf6dc36befd8780367c0b98927c3a9d2986
mattsteen14 <mattsteen14@me.com>
2024-09-10 11:28:59 +0100

    redditApi & store term correction

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import redditReducer from '../reddit/redditSlice';
 import subRedditReducer from '../reddit/subRedditSlice';
-import redditApiReducer from '../reddit/redditApiSlice';
-import redditApiSlice from '../reddit/redditApiSlice';
+import { redditApi } from '../reddit/redditApiSlice';
 
 export const store = configureStore({
   reducer: {
     reddit: redditReducer,
     subReddit: subRedditReducer,
-    redditApi: redditApiReducer,
+    [redditApi.reducerPath]: redditApi.reducer, // The reducer path for the API slice should match the reducerPath defined in redditApi
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(redditApiSlice.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(redditApi.middleware),
 });
--- src/reddit/redditApiSlice.js
+++ src/reddit/redditApiSlice.js
@@ -2,20 +2,23 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const API_ROOT = 'https://www.reddit.com';
 
-export const redditApiSlice = createApi({
-    reducerPath: 'redditApi',
+export const redditApi = createApi({
+    reducerPath: 'redditApi', // Unique key for slice in Redux store
     baseQuery: fetchBaseQuery({
         baseUrl: API_ROOT
     }),
     endpoints: (builder) => ({
+        // Fetch subreddit posts
         getSubredditPosts: builder.query({
             query: (subreddit) => `${subreddit}.json`,
-            transformResponse: (response) => response.data.children.map((post) => post.data),
+            transformResponse: (response) => response.data.children.map((post) => post.data), // Transform the post to match your needs
         }),
+        // Fetch subreddits
         getSubreddits: builder.query({
             query: () => '/subreddits.json',
             transformResponse: (response) => response.data.children.map((subreddit) => subreddit.data),
         }),
+        // Fetch post comments
         getPostComments: builder.query({
             query: (permalink) => `${permalink}.json`,
             transformResponse: (response) => response[1].data.children.map((comment) => comment.data),
@@ -23,10 +26,11 @@ export const redditApiSlice = createApi({
     }),
 });
 
+// Export auto-generated hooks for usage in functional components
 export const {
     useGetSubredditPostsQuery,
     useGetSubredditsQuery,
     useGetPostCommentsQuery,
-} = redditApiSlice;
+} = redditApi;
 
-export default redditApiSlice.reducer;
+export default redditApi.reducer;
